94453b977057a33d4502b5b2d670da74b23c9231
MolRue <mctothej@gmail.com>
2022-11-11 17:43:31 +0900

    uploading

--- src/ch06/CarEX.java
+++ src/ch06/CarEX.java
@@ -15,15 +15,21 @@ public class CarEX {
 		myCar.speed = 60; // 필드 답 변경
 		System.out.println("수정된 값: " + myCar.speed); 
 		System.out.println("-------------------");
-		Car car2 = new Car("자가용");
+		Car car2 = new Car("2호기");
 		System.out.println(car2.model);
 		System.out.println(car2.color);
 		
 		System.out.println("-------------------");
 		
-		Car car3 = new Car("자가용", "빨간색");
+		Car car3 = new Car("3호기", "빨간색");
 		System.out.println(car3.model);
 		System.out.println(car3.color);
+		System.out.println("-------------------");
+		
+		Car car4 = new Car("4호기");
+		System.out.println(car4.model);
+		System.out.println(car4.color);
+		System.out.println(car4.maxSpeed);
 		System.out.println("-------------------");
 	}
 
