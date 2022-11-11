1dd9754bc346839436ea68ad06a9efe04b607576
MolRue <mctothej@gmail.com>
2022-11-11 17:08:37 +0900

    javaup

---
+++ src/ch06/Car.java
@@ -0,0 +1,34 @@
+package ch06;
+
+public class Car {
+	// 1. 필드 선언 (필드 : 객체의 데이터를 저장)
+	String company;
+	String model;
+	String color;
+	int maxSpeed;
+	int speed;
+	// boolean start; 	//시동여부
+
+	// overloading : 생성자를 여러개 둘수가 있다.
+	
+	Car(String model) {
+//		this.model = model;
+		this(model, "은색", 250);
+	}
+	Car(String model, String color) {
+//		this.model = model;
+//		this.color = color;
+		this(model, color, 250);
+	}
+
+	// 자바에서는 생성자를 명시하지 않으면, 컴파일 과정에서 생성자를 명시해준다.
+	Car(String model, String color, int maxSpeed) {
+		this.model = model; 				// this : 이 해당 객체 자체를 의미한다.
+		this.color = color;					// 위에 생성된 객체에서 받아온 값이 할당이 된다.
+		this.maxSpeed = maxSpeed;
+		
+	}
+	
+	
+			
+}
---
+++ src/ch06/CarEX.java
@@ -0,0 +1,30 @@
+package ch06;
+
+public class CarEX {
+
+	public static void main(String[] args) {
+		//1. 객체 생성
+		Car myCar = new Car("그렌저", "검정", 250);
+		
+		System.out.println(myCar.company);
+		System.out.println(myCar.model);
+		System.out.println(myCar.color);
+		System.out.println(myCar.maxSpeed);
+		System.out.println(myCar.speed);
+		
+		myCar.speed = 60; // 필드 답 변경
+		System.out.println("수정된 값: " + myCar.speed); 
+		System.out.println("-------------------");
+		Car car2 = new Car("자가용");
+		System.out.println(car2.model);
+		System.out.println(car2.color);
+		
+		System.out.println("-------------------");
+		
+		Car car3 = new Car("자가용", "빨간색");
+		System.out.println(car3.model);
+		System.out.println(car3.color);
+		System.out.println("-------------------");
+	}
+
+}
